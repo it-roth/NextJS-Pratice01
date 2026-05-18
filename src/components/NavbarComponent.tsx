f57e55a227f28408e3adc35d764ff3181b7b186e
Leng Saroth <lengsaroth9@gmail.com>
2026-05-18 19:44:43 +0700

    update

--- src/components/NavbarComponent.tsx
+++ src/components/NavbarComponent.tsx
@@ -1,7 +1,7 @@
 export function Nav() {
   return (
 
-    <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+    <nav className="bg-black fixed w-full z-20 top-0 start-0 border-b border-default">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="https://flowbite.com/docs/images/logo.svg" className="h-7" alt="Flowbite Logo" />
